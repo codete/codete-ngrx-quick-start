142241436a150255a33e2bf8fbbfaa657795069b
Dariusz Filipiak <dariusz.filipiak@codete.com>
2023-05-26 14:15:17 +0200

    update

--- main/environment.js
+++ main/environment.js
@@ -3,7 +3,27 @@ const path = require('path')
 var { config } = { config: {} };
 
 config = {
-
+  loading: {
+    preAngularBootstrap: {
+      loader: {
+        name: 'lds-heart',
+        color: '#c75a59'
+      },
+      background: '#9395ab',
+    },
+    afterAngularBootstrap: {
+      loader: {
+        name: 'lds-default',
+        color: '#c85958'
+      },
+      background: '#868686',
+    },
+  },
+  entites: {
+    FiredevFile: {
+      dontLoadAssets: false,
+    }
+  },
   title: 'Codete NgRx Quick Start',
   pwa: {
     name: 'Codete NgRx Quick Start',
